8e3bddd8fca0940a34a92bb8d68d80db3f5013e9
tim-hub <tim_w@outlook.com>
2020-03-20 20:52:07 +1300

    fork all functions in

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import fcntl
 import itertools
-import ujson
-import requests
-from xml.etree import ElementTree
 from datetime import datetime
 from decimal import Decimal
-
-from sanic import Sanic
-from sanic.response import file, html, json, redirect
-from utils import parse_database_url, Gino as GinoUs, cors
 from os import getenv
-from gino.dialects.asyncpg import JSONB
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from xml.etree import ElementTree
 
-from constants import HISTORIC_RATES_URL, LAST_90_DAYS_RATES_URL
+import requests
+import ujson
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from gino import Gino
+from gino.dialects.asyncpg import JSONB
+from sanic import Sanic
+from sanic.response import json, redirect
+
+from constants import HISTORIC_RATES_URL, LAST_90_DAYS_RATES_URL, BASE_CURRENCY
+from utils import parse_database_url, cors
 
 app = Sanic(__name__)
 
@@ -27,6 +27,7 @@ app.config.update(
 # Database
 db = Gino()
 
+
 class ExchangeRates(db.Model):
     __tablename__ = "exchange_rates"
 
@@ -35,6 +36,7 @@ class ExchangeRates(db.Model):
 
     def __repr__(self):
         return "Rates [{}]".format(self.date)
+
 
 async def update_rates(historic=False):
     r = requests.get(HISTORIC_RATES_URL if historic else LAST_90_DAYS_RATES_URL)
@@ -56,6 +58,147 @@ async def update_rates(historic=False):
                     c.attrib["currency"]: Decimal(c.attrib["rate"]) for c in list(d)
                 },
             )
+
+
+async def to_exchange_rates(request, date=None):
+    if request.method == "HEAD":
+        return json("")
+
+    dt = datetime.now()
+    if date:
+        try:
+            dt = datetime.strptime(date, "%Y-%m-%d")
+        except ValueError as e:
+            return json({"error": "{}".format(e)}, status=400)
+
+        if dt < datetime(1999, 1, 4):
+            return json(
+                {"error": "There is no data for dates older then 1999-01-04."},
+                status=400,
+            )
+
+    exchange_rates = (
+        await ExchangeRates.query.where(ExchangeRates.date <= dt.date())
+            .order_by(ExchangeRates.date.desc())
+            .gino.first()
+    )
+    rates = exchange_rates.rates
+
+    # Base
+    base = BASE_CURRENCY
+    if "base" in request.raw_args and request.raw_args["base"] != "EUR":
+        base = request.raw_args["base"]
+
+        if base in rates:
+            base_rate = Decimal(rates[base])
+            rates = {
+                currency: Decimal(rate) / base_rate for currency, rate in rates.items()
+            }
+            rates["EUR"] = Decimal(1) / base_rate
+        else:
+            return json(
+                {"error": "Base '{}' is not supported.".format(base)}, status=400
+            )
+
+    # Symbols
+    if "symbols" in request.args:
+        symbols = list(
+            itertools.chain.from_iterable(
+                [symbol.split(",") for symbol in request.args["symbols"]]
+            )
+        )
+
+        if all(symbol in rates for symbol in symbols):
+            rates = {symbol: rates[symbol] for symbol in symbols}
+        else:
+            return json(
+                {
+                    "error": "Symbols '{}' are invalid for date {}.".format(
+                        ",".join(symbols), dt.date()
+                    )
+                },
+                status=400,
+            )
+
+    return json(
+        {"base": base, "date": exchange_rates.date.strftime("%Y-%m-%d"), "rates": rates}
+    )
+
+
+async def exchange_rates_history(request):
+    if request.method == "HEAD":
+        return json("")
+
+    if "start_at" in request.raw_args:
+        try:
+            start_at = datetime.strptime(request.raw_args["start_at"], "%Y-%m-%d")
+        except ValueError as e:
+            return json(
+                {"error": "start_at parameter format", "exception": "{}".format(e)},
+                status=400,
+            )
+    else:
+        return json({"error": "missing start_at parameter"})
+
+    if "end_at" in request.raw_args:
+        try:
+            end_at = datetime.strptime(request.raw_args["end_at"], "%Y-%m-%d")
+        except ValueError as e:
+            return json(
+                {"error": "end_at parameter format", "exception": "{}".format(e)},
+                status=400,
+            )
+    else:
+        return json({"error": "missing end_at parameter"})
+
+    exchange_rates = (
+        await ExchangeRates.query.where(ExchangeRates.date >= start_at.date())
+            .where(ExchangeRates.date <= end_at.date())
+            .order_by(ExchangeRates.date.asc())
+            .gino.all()
+    )
+
+    base = BASE_CURRENCY
+    historic_rates = {}
+    for er in exchange_rates:
+        rates = er.rates
+
+        if "base" in request.raw_args and request.raw_args["base"] != "EUR":
+            base = request.raw_args["base"]
+
+            if base in rates:
+                base_rate = Decimal(rates[base])
+                rates = {
+                    currency: Decimal(rate) / base_rate
+                    for currency, rate in rates.items()
+                }
+                rates["EUR"] = Decimal(1) / base_rate
+            else:
+                return json(
+                    {"error": "Base '{}' is not supported.".format(base)}, status=400
+                )
+
+        # Symbols
+        if "symbols" in request.args:
+            symbols = list(
+                itertools.chain.from_iterable(
+                    [symbol.split(",") for symbol in request.args["symbols"]]
+                )
+            )
+
+            if all(symbol in rates for symbol in symbols):
+                rates = {symbol: rates[symbol] for symbol in symbols}
+            else:
+                return json(
+                    {"error": "Symbols '{}' are invalid.".format(",".join(symbols))},
+                    status=400,
+                )
+
+        historic_rates[er.date] = rates
+
+    return json({"base": base, "start_at": start_at.date().isoformat(), "end_at": end_at.date().isoformat(),
+                 "rates": historic_rates})
+
 
 @app.listener("before_server_start")
 async def initialize_scheduler(app, loop):
@@ -84,6 +227,7 @@ async def initialize_scheduler(app, loop):
     except BlockingIOError:
         pass
 
+
 @app.middleware("request")
 async def force_ssl(request):
     if request.headers.get("X-Forwarded-Proto") == "http":
@@ -96,100 +240,54 @@ async def force_naked_domain(request):
         return redirect(request.url.replace("www.", "", 1), status=301)
 
 
-# @app.route("/latest", methods=["GET", "HEAD"])
-# @app.route("/<date>", methods=["GET", "HEAD"])
-# @app.route("/api/latest", methods=["GET", "HEAD"])
-# @app.route("/api/<date>", methods=["GET", "HEAD"])
-# @cors()
-# async def to_exchange_rates(request, date=None):
-    # if request.method == "HEAD":
-    #     return json("")
-    #
-    # dt = datetime.now()
-    # if date:
-    #     try:
-    #         dt = datetime.strptime(date, "%Y-%m-%d")
-    #     except ValueError as e:
-    #         return json({"error": "{}".format(e)}, status=400)
-    #
-    #     if dt < datetime(1999, 1, 4):
-    #         return json(
-    #             {"error": "There is no data for dates older then 1999-01-04."},
-    #             status=400,
-    #         )
-    #
-    # exchange_rates = (
-    #     await ExchangeRates.query.where(ExchangeRates.date <= dt.date())
-    #     .order_by(ExchangeRates.date.desc())
-    #     .gino.first()
-    # )
-    # rates = exchange_rates.rates
-    #
-    # # Base
-    # base = "EUR"
-    # if "base" in request.raw_args and request.raw_args["base"] != "EUR":
-    #     base = request.raw_args["base"]
-    #
-    #     if base in rates:
-    #         base_rate = Decimal(rates[base])
-    #         rates = {
-    #             currency: Decimal(rate) / base_rate for currency, rate in rates.items()
-    #         }
-    #         rates["EUR"] = Decimal(1) / base_rate
-    #     else:
-    #         return json(
-    #             {"error": "Base '{}' is not supported.".format(base)}, status=400
-    #         )
-    #
-    # # Symbols
-    # if "symbols" in request.args:
-    #     symbols = list(
-    #         itertools.chain.from_iterable(
-    #             [symbol.split(",") for symbol in request.args["symbols"]]
-    #         )
-    #     )
-    #
-    #     if all(symbol in rates for symbol in symbols):
-    #         rates = {symbol: rates[symbol] for symbol in symbols}
-    #     else:
-    #         return json(
-    #             {
-    #                 "error": "Symbols '{}' are invalid for date {}.".format(
-    #                     ",".join(symbols), dt.date()
-    #                 )
-    #             },
-    #             status=400,
-    #         )
-    #
-    # return json(
-    #     {"base": base, "date": exchange_rates.date.strftime("%Y-%m-%d"), "rates": rates}
-    # )
-    #
-    # return json({"hello": "world"})
-
-#
-# @app.route("/")
-# async def test(request):
-#   return json({"hello": "world"})
+@app.route("/latest", methods=["GET", "HEAD"])
+@cors()
+async def exchange_rates(request, date=None):
+    return await to_exchange_rates(request, date)
 
 
+@app.route("/<date>", methods=["GET", "HEAD"])
+@cors()
+async def exchange_rates(request, date=None):
+    return await to_exchange_rates(request, date)
 
 
+@app.route("/api/latest", methods=["GET", "HEAD"])
+@cors()
+async def exchange_rates(request, date=None):
+    return await to_exchange_rates(request, date)
 
+
+@app.route("/api/<date>", methods=["GET", "HEAD"])
+@cors()
+async def exchange_rates(request, date=None):
+    return await to_exchange_rates(request, date)
+
+
+@app.route("/history", methods=["GET", "HEAD"])
+@cors()
+async def exchange_history(request):
+    return await exchange_rates_history(request)
+
+
+@app.route("/api/history", methods=["GET", "HEAD"])
+@cors()
+async def exchange_history(request):
+    return await exchange_rates_history(request)
 
 
 # api.ExchangeratesAPI.io
-@app.route("/", methods=["GET"], host="api.exchangeratesapi.io")
+@app.route("/", methods=["GET"])
 async def index(request):
-    return json({"details": "https://exchangeratesapi.io"}, escape_forward_slashes=False)
+    return json({"details": "https://tim.bai.uno/currency"}, escape_forward_slashes=False)
 
 
 # Website
-@app.route("/", methods=["GET", "HEAD"])
-async def index(request):
-    if request.method == "HEAD":
-        return html("")
-    return await file("./templates/index.html")
+# @app.route("/", methods=["GET", "HEAD"])
+# async def index(request):
+#     if request.method == "HEAD":
+#         return html("")
+#     return await file("./templates/index.html")
 
 
 # Static content
@@ -197,6 +295,5 @@ app.static("/static", "./static")
 app.static("/robots.txt", "./static/robots.txt")
 app.static("/favicon.ico", "./static/favicon.ico")
 
-
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
